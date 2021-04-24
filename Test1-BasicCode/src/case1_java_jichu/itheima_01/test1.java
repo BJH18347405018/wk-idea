686b22561ca0fbfc68a28d02e11b29dcd6c25c37
小黄人 爱学习 <8999731+little-yellow-loves-to-learn@user.noreply.gitee.com>
2021-04-24 20:09:55 +0800

    Default Changelist

--- Test1-BasicCode/src/case1_java_jichu/itheima_01/test1.java
+++ Test1-BasicCode/src/case1_java_jichu/itheima_01/test1.java
@@ -2,7 +2,7 @@ package case1_java_jichu.itheima_01;
 
 public class test1 {
     public static void main(String[] args) {
-        int c = 100;
+        int c = 10;
         System.out.println(c);
     }
 
